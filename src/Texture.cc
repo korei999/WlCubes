5591a9911424f4c00af3c7d3f3de17e5196defe3
korei999 <ju7t1xe@gmail.com>
2024-07-28 23:50:37 +0300

    bring _mm_loadu_si128 back

--- src/Texture.cc
+++ src/Texture.cc
@@ -1,5 +1,4 @@
-#include <emmintrin.h>
-#include <smmintrin.h>
+#include <immintrin.h>
 
 #include "Texture.hh"
 #include "Arena.hh"
@@ -311,7 +310,7 @@ loadBMP(adt::Allocator* pAlloc, adt::String path, bool flip)
 }
 
 #ifdef DEBUG
-static void
+[[maybe_unused]] static void
 printPack(adt::String s, __m128i m)
 {
     u32 f[4];
@@ -341,7 +340,7 @@ flipCpyBGRAtoRGBA(u8* dest, u8* src, int width, int height, bool vertFlip)
     {
         for (int x = 0; x < width; x += 4)
         {
-            __m128i pack = _mm_load_si128((__m128i*)(&s[y*width + x]));
+            __m128i pack = _mm_loadu_si128((__m128i*)(&s[y*width + x]));
             __m128i redBits = _mm_and_si128(pack, _mm_set1_epi32(0x00'ff'00'00));
             __m128i blueBits = _mm_and_si128(pack, _mm_set1_epi32(0x00'00'00'ff));
             pack = _mm_and_si128(pack, _mm_set1_epi32(0xff'00'ff'00));
